b37973774e20a39af283436004d67362242a1d47
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-04 10:08:16 +0200

    Correction probleme du boutton

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+
 import "./style.scss";
 
 export const BUTTON_TYPES = {
@@ -6,44 +7,53 @@ export const BUTTON_TYPES = {
   SUBMIT: 2,
 };
 
-const Button = ({ 
-  title = "", 
-  onClick = () => null, 
-  type = BUTTON_TYPES.DEFAULT, 
-  disabled = false, 
-  children = null 
-}) => {
-  if (type === BUTTON_TYPES.SUBMIT) {
-    return (
-      <input
-        disabled={disabled}
-        className="Button"
-        type="submit"
-        data-testid="button-test-id"
-        value={title}
-        onClick={onClick}
-        title={title}
-      />
-    );
+const Button = ({ title="", onClick = null, type = BUTTON_TYPES.DEFAULT, disabled=false, children=null }) => {
+  switch (type) {
+    case BUTTON_TYPES.DEFAULT:
+      return (
+        <button
+          type="button"
+          disabled={disabled}
+          className="Button"
+          data-testid="button-test-id"
+          onClick={onClick}
+          title={title}
+        >
+          {children}
+        </button>
+      );
+    case BUTTON_TYPES.SUBMIT:
+      return (
+        <input
+          disabled={disabled}
+          className="Button"
+          type="submit"
+          data-testid="button-test-id"
+          value={children}
+          onClick={onClick}
+          title={title}
+        />
+      );
+    default:
+      return (
+        <button
+          type="button"
+          disabled={disabled}
+          className="Button"
+          data-testid="button-test-id"
+          onClick={onClick}
+          title={title}
+        >
+          {children}
+        </button>
+      );
   }
-
-  return (
-    <button
-      type="button"
-      disabled={disabled}
-      className="Button"
-      data-testid="button-test-id"
-      onClick={onClick}
-      title={title}
-    >
-      {children || title}
-    </button>
-  );
 };
 
+// eslint-disable-next-line react/no-typos
 Button.propTypes = {
-  title: PropTypes.string , // eslint-disable-line react/require-default-props
-  onClick: PropTypes.func, // eslint-disable-line react/require-default-props
+  title: PropTypes.string,// eslint-disable-line react/require-default-props
+  onClick: PropTypes.func,// eslint-disable-line react/require-default-props
   type: PropTypes.number, // eslint-disable-line react/require-default-props
   disabled: PropTypes.bool, // eslint-disable-line react/require-default-props
   children: PropTypes.node, // eslint-disable-line react/require-default-props
