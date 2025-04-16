5b02826cb00cd67148cbdd3d222e574039833065
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-16 09:16:22 +0200

    Modification test d'integration

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,6 +1,44 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
 import EventCard from "../../components/EventCard";
+import Eventlist from "../../containers/Events";
+import { api, DataProvider } from "../../contexts/DataContext";
+
+const data = {
+  events: [
+    {
+      id: 1,
+      type: "conférence",
+      date: "2022-04-29T20:28:45.744Z",
+      title: "User&product MixUsers",
+      cover: "/images/alexandre-pellaes-6vAjp0pscX0-unsplash.png",
+      description: "Présentation des nouveaux usages UX.",
+      nb_guesses: 900,
+      periode: "14-15-16 Avril",
+      prestations: [
+        "1 espace d’exposition",
+        "1 scéne principale",
+        "1 espace de restaurations",
+      ],
+    },
+    {
+      id: 2,
+      type: "expérience digitale",
+      date: "2022-01-29T20:28:45.744Z",
+      title: "#DigitonPARIS",
+      cover: "/images/charlesdeluvio-wn7dOzUh3Rs-unsplash.png",
+      description:
+        "Présentation des outils analytics aux professionnels du secteur ",
+      nb_guesses: 1300,
+      periode: "24-25-26 Février",
+      prestations: [
+        "1 espace d’exposition",
+        "1 scéne principale",
+        "1 site web dédié",
+      ],
+    },
+  ],
+};
 
 describe("When Form is created", () => {
   it("a list of fields card is displayed", async () => {
@@ -30,8 +68,14 @@ describe("When Form is created", () => {
 describe("When a page is created", () => {
   it("a list of events is displayed", async () => {
     window.console.error = jest.fn();
-    render(<Home />);
+    api.loadData = jest.fn().mockReturnValue(data);
+    render(
+      <DataProvider>
+        <Eventlist />
+      </DataProvider>
+    );
     await screen.findByTestId("listeEvents");
+    await screen.findByText("#DigitonPARIS")
   });
   it("a list a people is displayed", async () => {
     window.console.error = jest.fn();
@@ -45,8 +89,7 @@ describe("When a page is created", () => {
   });
   it("an event card, with the last event, is displayed", async () => {
     window.console.error = jest.fn();
-    render(
-    <EventCard />); // Avec Home cela ne marche pas et je comprends pas le dernier event
+    render(<EventCard />);
     await screen.findByTestId("card-testid");
   });
 });
